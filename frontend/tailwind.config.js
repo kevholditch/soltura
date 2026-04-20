50d59f30e7cf40f6b2b29c6bc09854a2c2be13a4
Kevin Holditch <kevholditch@hotmail.com>
2026-04-20 15:25:58 +0100

    chore: scaffold React+Vite frontend with Tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        terracotta: '#C1440E',
+        saffron: '#F5A623',
+      },
+    },
+  },
+  plugins: [],
+}
